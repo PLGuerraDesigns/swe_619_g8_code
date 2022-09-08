b4ff1e8c88b21a05e07e9ed9b7b594f96bb7e23b
Pablo Guerra <pguerra_16@me.com>
2022-09-08 13:04:42 -0400

    Assignment 2 Starter Code

---
+++ assignment_2/Drill.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Drill {
+
+    public static boolean match(Map<String, String> m, String prompt, String answer) {
+        // Requires: m, prompt, answer not null; prompt is a key in Map m, ignoring case
+        // Effects: returns true iff the Map m maps prompt to answer, ignoring case
+
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
