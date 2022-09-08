f915ecdc1a27bb94372e31e4b186545f60559748
Pablo Guerra <pguerra_16@me.com>
2022-09-08 13:10:02 -0400

    Preconditions implementation

--- assignment_2/Drill.java
+++ assignment_2/Drill.java
@@ -6,10 +6,33 @@ public class Drill {
         // Requires: m, prompt, answer not null; prompt is a key in Map m, ignoring case
         // Effects: returns true iff the Map m maps prompt to answer, ignoring case
 
+        // TODO: Modify so that it handles all of its preconditions with exceptions. Use
+        // the standard exceptions recommended by Bloch. Document this with a revised
+        // contract. You can use JavaDoc or you can simply identify the postconditions.
+
+        if (m.get(prompt.toLowerCase()).equals(answer.toLowerCase())) {
+            return true;
+        }
         return false;
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+
+        // Creating new map and populating with content
+        Map<String, String> m = new HashMap<String, String>();
+        m.put("dog", "chien");
+        m.put("cat", "chat");
+        m.put("bird", "oiseau");
+        m.put("pig", "porc");
+
+        // Tests
+        boolean result = match(m, "pig", "oiseau");
+        System.out.println("pig = oiseau: " + result);
+
+        result = match(m, "dog", "chien");
+        System.out.println("dog = chien: " + result);
+
+        result = match(m, "DoG", "CHieN");
+        System.out.println("DoG = CHieN: " + result);
     }
 }
