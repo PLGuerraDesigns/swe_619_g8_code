52a1fbe3c2feeaaa034e0ffb6bded3b47aa3ff55
yang9501 <yang9501@users.noreply.github.com>
2022-09-11 18:49:08 -0400

    Broke tests into individual

--- assignment_2/src/DrillTest.java
+++ assignment_2/src/DrillTest.java
@@ -1,31 +1,59 @@
 package src;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DrillTest {
-    @Test
-    void testDrill(){
-        //define default map
-        Map<String, String>testMap = new HashMap<String, String>();
+
+    //define default map
+    private Map<String,String> testMap = new HashMap<String,String>();
+
+    @BeforeAll
+    void setup() {
         testMap.put("dog", "chien");
         testMap.put("cat", "chat");
-        testMap.put("bird", "oiseau");
-        testMap.put("pig", "porc");
+        testMap.put("bIRd", "oISeau");
+        testMap.put("PIG", "PORC");
+    }
 
+    @Test
+    void testDrill(){
         //test correct test cases
         assertTrue(Drill.matchExceptionized(testMap, "dog", "chien"));
-        assertTrue(Drill.matchExceptionized(testMap, "cat", "chat"));
+        assertTrue(Drill.matchExceptionized(testMap, "cat", "CHAT"));
         assertTrue(Drill.matchExceptionized(testMap, "bIRd", "oISeau"));
         assertTrue(Drill.matchExceptionized(testMap, "pIG", "pOrC"));
+    }
 
-        //test illegal argument exceptions
-        assertThrows(NullPointerException.class, () -> Drill.matchExceptionized(testMap, null, null),"test");
+    @Test
+    //Testing when the map is null
+    void testMNull() {
+        assertThrows(NullPointerException.class, () -> Drill.matchExceptionized(null, "dog", "chien"),"test");
+    }
+
+    @Test
+    //Testing when "prompt" input is null
+    void testPromptNull() {
+        assertThrows(NullPointerException.class, () -> Drill.matchExceptionized(testMap, null, "chien"),"test");
+    }
+
+    @Test
+    //Testing when "answer" input is null
+    void testAnswerNull() {
+        assertThrows(NullPointerException.class, () -> Drill.matchExceptionized(testMap, "dog", null),"test");
+    }
+
+    @Test
+    //Testing if "prompt" is not present in the keys of "m"
+    void testKeyNotPresent() {
         assertThrows(IllegalArgumentException.class, () -> Drill.matchExceptionized(testMap, "horse", "chat"),"test");
     }
 }
