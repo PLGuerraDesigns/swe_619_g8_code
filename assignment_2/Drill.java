b3894235c50f75e4eb0534bdb17cb86329ab4ac7
yang9501 <yang9501@users.noreply.github.com>
2022-09-11 13:32:47 -0400

    Exceptions WIP, checkpointing

--- assignment_2/Drill.java
+++ assignment_2/Drill.java
@@ -8,7 +8,9 @@ public class Drill {
 
         //There is no case-insensitive exclusivity clause for the key in m, the result will be undeterministic:
         // If two keys “dog” and “DOG” both exist in m, this function will only return the value associated with one of them
-        // Furthermore, if two keys "dog" and "DOG" both exist in m, "dog" is first to be compared, and "dog"'s corresponding value is different from answer, this will return false
+        // Furthermore, if two keys "dog" and "DOG" both exist in m, "dog" is first to be compared,
+            // and "dog"'s corresponding value is different from answer,
+            // this will return false even though there may be a matching pair further down in the map
 
         //Possible Courses of action
         //1. CURRENTLY USING THIS: Iterate through the map and manually compare the keys. https://www.baeldung.com/java-iterate-map
@@ -23,19 +25,56 @@ public class Drill {
         return false;
     }
 
+    public static boolean matchExceptionized(Map<String, String> m, String prompt, String answer) {
+        // Requires: m, prompt, answer not null; prompt is a key in Map m, ignoring case
+        // Effects: returns true iff the Map m maps prompt to answer, ignoring case
+
+        if(m==null || prompt==null || answer==null) {
+            throw new NullPointerException("All inputs must have non-null values.");
+        }
+        /*  containsKey does not ignore case, this check won't work as currently written
+        if(!m.containsKey(prompt)) {
+            throw new IllegalArgumentException("prompt is not a key in m.");
+        }
+        */
+        for(Map.Entry<String,String> entry: m.entrySet()) {
+            if(entry.getKey().equalsIgnoreCase(prompt)) {
+                return entry.getValue().equalsIgnoreCase(answer);
+            }
+        }
+        //lol, this technically works i think
+        throw new IllegalArgumentException("prompt is not a key in m.");
+        //return false;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("BEGINNING TESTS...");
 
         Map<String,String> testMap = new HashMap<String,String>();
         testMap.put("dog","le chien");
-
         String prompt = "";
         String answer = "";
-
         System.out.println("MAP CONSISTS OF: " + testMap);
         //Case 1:
         prompt = "DoG";
         answer = "LE cHiEn";
         System.out.println("Case 1: Prompt = " + prompt + ", Answer = " + answer + ", Result = " + match(testMap,prompt, answer));
+        System.out.println("-----------------------------------------------------");
+        System.out.println("TESTING match() WITH EXCEPTIONS...");
+        Map<String,String> testMapExceptions = new HashMap<String,String>();
+        testMapExceptions.put("dog","le chien");
+        String promptE = "";
+        String answerE = "";
+        System.out.println("MAP CONSISTS OF: " + testMapExceptions);
+        //Case 1:
+        promptE = "DoG";
+        answerE = "LE cHiEn";
+        System.out.println("Case 1: Prompt = " + promptE + ", Answer = " + answerE + ", Result = " + matchExceptionized(testMapExceptions,promptE, answerE));
+
+        //Case 2:
+        promptE = null;
+        answerE = null;
+        System.out.println("Case 2: Prompt = " + promptE + ", Answer = " + answerE + ", Result = " + matchExceptionized(testMapExceptions,promptE, answerE));
+
     }
 }
