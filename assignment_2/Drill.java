5fe2f5d84711627dbeb95d128bc44f9b0e3ce726
yang9501 <yang9501@users.noreply.github.com>
2022-09-11 13:17:33 -0400

    Basic implementation of Part 1, comments for clarity and possible COAs

--- assignment_2/Drill.java
+++ assignment_2/Drill.java
@@ -6,15 +6,21 @@ public class Drill {
         // Requires: m, prompt, answer not null; prompt is a key in Map m, ignoring case
         // Effects: returns true iff the Map m maps prompt to answer, ignoring case
 
-        //There is no exclusivity clause for the key in m, the result will be undeterministic:
+        //There is no case-insensitive exclusivity clause for the key in m, the result will be undeterministic:
         // If two keys “dog” and “DOG” both exist in m, this function will only return the value associated with one of them
+        // Furthermore, if two keys "dog" and "DOG" both exist in m, "dog" is first to be compared, and "dog"'s corresponding value is different from answer, this will return false
 
-        //THIS IMPLEMENTATION DOES NOT WORK.  It fails to case-desensitize the "prompt" input.
-        //1. Iterate through the map and manually compare the keys
+        //Possible Courses of action
+        //1. CURRENTLY USING THIS: Iterate through the map and manually compare the keys. https://www.baeldung.com/java-iterate-map
         //2. Use TreeMap w/ case insensitive comparator: https://codereview.stackexchange.com/questions/183127/map-with-case-insensitive-get-remove-and-containskey
             //Would this implementation be expected to work with all types of maps? If so, the above COA will not work.
-        //3. Drill takes a Map<String,String> and creates a new copy constructor'd map that has each of the keys .toLowerCase()'ed.  O(n) construction time, O(1) lookup
-        return m.get(prompt).equalsIgnoreCase(answer);
+        //3. Drill takes a Map<String,String> and creates a new copy constructor'd map that has each of the keys .toLowerCase()'ed.  O(n) construction time, O(1) lookup.
+        for(Map.Entry<String,String> entry: m.entrySet()) {
+            if(entry.getKey().toLowerCase().equalsIgnoreCase(prompt)) {
+                return entry.getValue().equalsIgnoreCase(answer);
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) throws Exception {
