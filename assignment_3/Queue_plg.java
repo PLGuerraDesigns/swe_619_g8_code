921cf57a1d10475e4355728c1969354af74b58a5
Pablo Guerra <pguerra_16@me.com>
2022-09-18 19:02:18 -0400

    Updated Class Name

--- assignment_3/Queue_plg.java
+++ assignment_3/Queue_plg.java
@@ -1,35 +1,35 @@
 
 import java.util.*;
 
-public class Queue<E> {
+public class Queue_plg<E> {
 
     private final List<E> elements;
     private final int size;
 
-    private Queue() {
+    private Queue_plg() {
         this.elements = new ArrayList<E>();
         this.size = 0;
     }
 
-    private Queue(E e, List<E> elements) {
+    private Queue_plg(E e, List<E> elements) {
         if (e != null)
             elements.add(e);
         this.elements = elements;
         this.size = elements.size();
     }
 
-    public Queue<E> enQueue(E e) {
-        return new Queue<E>(e, this.elements);
+    public Queue_plg<E> enQueue(E e) {
+        return new Queue_plg<E>(e, this.elements);
     }
 
     public E top() {
         return this.elements.get(0);
     }
 
-    public Queue<E> deQueue() {
+    public Queue_plg<E> deQueue() {
         if (size == 0)
             throw new IllegalStateException("Queue.deQueue");
-        return new Queue<E>(null, this.elements.subList(1, this.size));
+        return new Queue_plg<E>(null, this.elements.subList(1, this.size));
     }
 
     public boolean isEmpty() {
@@ -40,7 +40,7 @@ public class Queue<E> {
         // Simple exercise to enQueue/deQueue cmd line args
         // Usage: java Queue item1 item2 item3 ...
 
-        Queue<String> q = new Queue<String>();
+        Queue_plg<String> q = new Queue_plg<String>();
 
         // for (String arg : args)
         // q.enQueue(arg);
