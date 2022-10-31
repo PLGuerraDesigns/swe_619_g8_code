f75aa2a97df9d032230030d4fca3affd7628fdbb
Pablo Guerra <pguerra_16@me.com>
2022-10-31 18:19:47 -0400

    Updated comments - inclass_9

--- inclass_9/IntSet.java
+++ inclass_9/IntSet.java
@@ -1,5 +1,15 @@
 package inclass_9;
 
+// How should the equals() method be completed?
+// Analyze the following ways to implement hashCode()? If there is a problem,
+// give a test case that shows the problem.
+//       not overridden at all
+//       return 42;
+//       return els.hashCode();
+//       int sum = 0; for (Integer i : els) sum += i.hashCode(); return sum;
+// What's the problem with clone() here? Give a test case that shows the problem.
+// Fix clone() in two very different ways.
+
 public class IntSet implements Cloneable {
    private List<Integer> els;
 
@@ -30,15 +40,3 @@ public class IntSet implements Cloneable {
       return new IntSet(new ArrayList<Integer>(els));
    }
 }
-
-// How should the equals() method be completed?
-// Analyze the following ways to implement hashCode()? If there is a problem,
-// give a test case that shows the problem.
-// not overridden at all
-// return 42;
-// return els.hashCode();
-// int sum = 0; for (Integer i : els) sum += i.hashCode(); return sum;
-// What's the problem
-
-// with clone() here? Give a test case that shows the problem.
-// Fix clone() in two very different ways.
